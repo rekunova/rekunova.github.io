bcd1336def3c504e5cdba6a040ee06baddc7968b
rekunova <ma.rekunova@gmail.com>
2019-09-03 16:06:12 +0300

    adaptiveFont

--- words.js
+++ words.js
@@ -9,36 +9,36 @@ const words = [
         en: "vulnerable",
         translation: "уязвимый"
     },
-    {
-        id: "3",
-        en: "shank",
-        translation: "голень"
-    },
-    {
-        id: "4",
-        en: "tease",
-        translation: "дразнить"
-    },
-    {
-        id: "5",
-        en: "efface",
-        translation: "стирать, вычеркивать"
-    },
+    // {
+    //     id: "3",
+    //     en: "shank",
+    //     translation: "голень"
+    // }
+    // {
+    //     id: "4",
+    //     en: "tease",
+    //     translation: "дразнить"
+    // },
+    // {
+    //     id: "5",
+    //     en: "efface",
+    //     translation: "стирать, вычеркивать"
+    // },
     {
         id: "6",
         en: "disclose",
         translation: "обнаруживать"
-    },
-    {
-        id: "7",
-        en: "midget",
-        translation: "карлик"
-    },
-    {
-        id: "8",
-        en: "soothsayer",
-        translation: "предсказатель"
     }
+    // {
+    //     id: "7",
+    //     en: "midget",
+    //     translation: "карлик"
+    // },
+    // {
+    //     id: "8",
+    //     en: "soothsayer",
+    //     translation: "предсказатель"
+    // }
 ]
 
 export default words;
